b492979f08dfa7bdfca0b574152ee74ec0b15f9d
Calancea Daniel <d4no0007@gmail.com>
2023-10-28 12:03:06 +0300

    Added a new elixir project

---
+++ lib/zig_builder.ex
@@ -0,0 +1,18 @@
+defmodule ZigBuilder do
+  @moduledoc """
+  Documentation for `ZigBuilder`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ZigBuilder.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/zig_builder_test.exs
@@ -0,0 +1,8 @@
+defmodule ZigBuilderTest do
+  use ExUnit.Case
+  doctest ZigBuilder
+
+  test "greets the world" do
+    assert ZigBuilder.hello() == :world
+  end
+end
